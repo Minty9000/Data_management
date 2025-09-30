e973987419cc33ac58273e550c040b7b5720c50b
Mani <manivardhan.k03@gmail.com>
2025-09-30 16:28:25 -0400

    Added menu options

--- movie_recommender.py
+++ movie_recommender.py
@@ -8,3 +8,14 @@ rating_df = pd.read_csv("sample_ratings.txt", sep="|", header=None, names=["movi
 print(movies_df.head())
 print(rating_df.head())
 
+menu_options = """
+Select an option:
+1. Import movies dataset
+2. Import ratings dataset
+3. Show top N movies (overall)
+4. Show top N movies by genre
+5. Show your most preferred genre
+6. Show 3 most popular movies from your favorite genre
+7. Exit program
+"""
+
